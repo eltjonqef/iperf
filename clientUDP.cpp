8eb554e5cdd4cb98fa95db732a043d053ca9de1c
Eltion <eltion.kef98@gmail.com>
2020-04-09 08:34:51 +0300

    working without throttling

--- clientUDP.cpp
+++ clientUDP.cpp
@@ -13,29 +13,14 @@
 
 using namespace std;
 
-map<int, uint8_t*> headerC;
-map<uint8_t*, int> headerLenC;
 
 int main(){
-    uint8_t header[67010];
-    header[0]='i';
-    header[1]='p';
-    header[2]='e';
-    header[3]='r';
-    header[4]='f';
-    header[5]='m';
-    header[6]='s';
-    header[7]='g';
-    //header[8] packetCounter Byte2
-    //header[9] packetCounter Byte1
-
-
-
+    
+    uint8_t *data=(uint8_t*)malloc(DATALIMIT*sizeof(sizeof(uint8_t)));
+    FILE* fd = fopen("/dev/urandom", "rb");
+    fread(data,sizeof(uint8_t),DATALIMIT,fd);
+    fclose(fd);
     struct sockaddr_in serverInfo;
-
-    
-    
-
     int sock;
     if((sock=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1){
         perror("opening UDP socket");
@@ -46,45 +31,9 @@ int main(){
     serverInfo.sin_family = AF_INET; 
     serverInfo.sin_addr.s_addr = INADDR_ANY;
     serverInfo.sin_port = htons(PORT); 
-    
-
-    int counter=1;
-    struct timespec now;
-    while(1){ //SEND DATA EVERY SECOND
-        int next=DATALIMIT;
-        int dataSent=0;
-        while(dataSent<BANDWIDTH){ //THIS IS TO REACH THE BIT RATE IN THAT SECOND
-            if(BANDWIDTH-dataSent<DATALIMIT){
-                next=BANDWIDTH-dataSent;
-            }
-            dataSent+=next;
-            header[8]=(counter >> 8) & 0xFF; //BYTE SPLIT SE 2 BYTES GIA NA BOREI NA KRATAEI MEXRI
-            header[9]=counter & 0xFF;        // KAI TON ARITHMO 65535 //MPOREI NA TO AUKSISOUME AMA XREIASTEI
-            header[10]=(next >> 8) & 0xFF;
-            header[11]=next & 0xFF;
-            /*clock_gettime(CLOCK_REALTIME, &now);
-            header[12]=(now.tv_sec >>24)& 0xFF;
-            header[13]=(now.tv_sec >>16)& 0xFF;
-            header[14]=(now.tv_sec >>8)& 0xFF;
-            header[15]=now.tv_sec & 0xFF;
-
-            header[16]=(now.tv_nsec >>24)& 0xFF;
-            header[17]=(now.tv_nsec >>16)& 0xFF;
-            header[18]=(now.tv_nsec >>8)& 0xFF;
-            header[19]=now.tv_nsec & 0xFF;
-            cout<<"TEST S:"<<now.tv_sec<<" Test N:"<<now.tv_nsec<<endl;*/
-            uint8_t *data=(uint8_t*)malloc((next)*sizeof(sizeof(uint8_t)));
-            FILE* fd = fopen("/dev/urandom", "rb");
-            fread(data,sizeof(uint8_t),next,fd);
-            fclose(fd);
-            for(int i=0; i<(next-12); i++){
-                header[i+12]=data[i];
-            }
-            sendto(sock, header, next, MSG_WAITALL, (struct sockaddr*)&serverInfo,sizeof(serverInfo));
-            counter++;
-            free(data);
-        }
-        //usleep(5000000); //SLEEP 1 SECOND
+    while(1){
+        sendto(sock, data, DATALIMIT, MSG_WAITALL, (struct sockaddr*)&serverInfo,sizeof(serverInfo));
     }
+    int counter=1;
     return 0;
 }
--- serverUDP.cpp
+++ serverUDP.cpp
@@ -19,7 +19,7 @@ mutex increment;
 void throughput(){
     usleep(1000000);
     increment.lock();
-    cout<<receivedBytes*8/1024/1024<<endl;
+    cout<<(double)receivedBytes*8/1024/1024/1024<<endl;
     receivedBytes=0;
     increment.unlock();
     throughput();
@@ -42,52 +42,11 @@ int main(){
     }
     
     socklen_t len=sizeof(clientInfo);
-    int counter=0;
-    int clientPacketCounter=0;
-    int serverPacketCounter=0;
-    double previous, newS;
-    struct timespec now;
-    uint32_t startSeconds,startNSeconds, finishSeconds,finishNSeconds;
+
     thread first (throughput);
     uint8_t *buffer=(uint8_t*)malloc(65535*sizeof(uint8_t));
     while(1){
-        //size_t receivedBytes=0;
-        //while(1){
-            
-            /*if(firstPacket==1){
-                firstPacket=0;
-                flag=1;
-            }*/
             receivedBytes+=recvfrom(sock, buffer, 65535, MSG_WAITALL,(struct sockaddr *)&clientInfo, &len);
-            /*if(flag==1){
-                startSeconds=buffer[12];
-                startSeconds=(startSeconds<<8)| buffer[13];
-                startSeconds=(startSeconds<<8)| buffer[14];
-                startSeconds=(startSeconds<<8)| buffer[15];
-                startNSeconds=buffer[16];
-                startNSeconds=(startNSeconds<<8)| buffer[17];
-                startNSeconds=(startNSeconds<<8)| buffer[18];
-                startNSeconds=(startNSeconds<<8)| buffer[19]; 
-
-                flag=0;
-            }*/
-            serverPacketCounter++;
-            //if(((buffer[10] << 8) | buffer[11])!=65507){
-                /*clock_gettime(CLOCK_REALTIME, &now);
-                finishSeconds=now.tv_sec;
-                finishNSeconds=now.tv_nsec;
-                clientPacketCounter=(buffer[8] << 8) | buffer[9];*/
-               // break;
-            //}
-            //free(buffer);
-        //}
-        /*cout<<"Goodput: "<<endl; 
-        double delta=(finishSeconds-startSeconds)+1.0e-9*(finishNSeconds-startNSeconds);
-        cout<<receivedBytes<<endl;
-        cout<<delta<<endl;
-        cout<<(receivedBytes/delta)/1000<<endl;
-        //cout<<"Jitter: "<<(double)(clockEnd.tv_sec - clockStart.tv_sec)+1.0e-9*(clockEnd.tv_nsec - clockStart.tv_nsec)<<endl;
-        cout<<"Lost: "<<clientPacketCounter -serverPacketCounter<<"/"<<serverPacketCounter<<endl;*/
     }
     return 0;
 }
